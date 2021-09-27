5bde2241e0fc64e97fb9940a24815f97f34e20fb
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-09-27 00:18:27 -0600

    Update invbinarytree.js

--- invbinarytree.js
+++ invbinarytree.js
@@ -6,7 +6,7 @@ const invertTree = function(root) {
 
 /* Algo:
 All we have to do is to invert the nodes in a binary tree, 
-so we will perform a normal tree traversal but interchnage the value of left node with the value of right node and vice-versa and save it as  new TreeNode.
+so we will perform a normal tree traversal but interchange the value of left node with the value of right node and vice-versa and save it as  new TreeNode.
 
 
 
