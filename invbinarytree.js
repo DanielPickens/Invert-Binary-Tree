3ea72588870a72beca402a614f8ad3bb8d4cd0ae
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-09-27 00:11:12 -0600

    Update invbinarytree.js

--- invbinarytree.js
+++ invbinarytree.js
@@ -2,3 +2,10 @@ const invertTree = function(root) {
     if(root == null || (root.left == null && root.right == null)) return root;
         return new TreeNode(root.val, invertTree(root.right), invertTree(root.left));
     }
+
+
+
+/*
+Runtime: 72 ms, faster than 82.58% of JavaScript online submissions for Invert Binary Tree.
+Memory Usage: 40.1 MB, less than 35.21% of JavaScript online submissions for Invert Binary Tree.
+*/
